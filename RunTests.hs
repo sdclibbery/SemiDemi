a7eb771e9d5f0e7292560bfb6ed6de5e5d665502
sdclibbery <sdclibbery@googlemail.com>
2014-07-13 17:30:16 +0100

    Tweak example test

--- RunTests.hs
+++ RunTests.hs
@@ -2,7 +2,12 @@ import Test.HUnit
 import qualified MatcherTests
 
 {- TODO:
-* Use lev over all prefixes; pick where editdistance is lowest
+* Need a new fuzzy matcher. Run Lev like algo ONCE. For the last match, it returns the editDistance+remainingNeedleLength and the remaining haystack
+* Rename Matcher to Scorer
+* Parser to generate ScorerDesc from marked up input string
+* Matcher to apply a collection of Scorers to an input string and pick the best
+* Tool to generate Descs etc from existing regression data
+* Rgeression suite
 -}
 
 main = runTestTT $ TestList
