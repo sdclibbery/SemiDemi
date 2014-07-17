9929cef51e3e2420d4c870ab0bd79a264955de42
sdclibbery <sdclibbery@googlemail.com>
2014-07-17 23:24:29 +0100

    Add Parser module

---
+++ Parser.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser (
+	parse
+) where
+import qualified Matcher as M
+import Data.ByteString.Char8
+import qualified Data.Attoparsec.ByteString.Char8 as P
+import Control.Applicative
+
+parse :: String -> (Either String M.Desc)
+parse t = P.parseOnly parseDesc $ pack t
+
+parseDesc :: P.Parser M.Desc
+parseDesc = do
+	fs <- P.many' (parseVersion <|> parseExact <|> parseFuzzy)
+	ds <- P.manyTill (parseDisallowed <|> fail "Parse error") P.endOfInput
+	return $ M.Desc fs ds
+
+parseFuzzy :: P.Parser M.Flow
+parseFuzzy = do
+	f <- P.many1 $ P.notChar '|'
+	return $ M.Fuzzy f
+
+parseExact :: P.Parser M.Flow
+parseExact = do
+	P.string "|+"
+	e <- P.many1 $ P.notChar '|'
+	P.string "|"
+	return $ M.Exact e
+
+parseVersion :: P.Parser M.Flow
+parseVersion = do
+	P.string "|v|"
+	return M.Version
+
+parseDisallowed :: P.Parser M.Disallowed
+parseDisallowed = do
+	P.string "|-"
+	d <- P.many1 $ P.notChar '|'
+	P.string "|"
+	return $ M.Disallowed d
+
--- RunTests.hs
+++ RunTests.hs
@@ -1,14 +1,14 @@
 import Test.HUnit
 import qualified MatcherTests
+import qualified ParserTests
 
 {- TODO:
-* Still not fast enough :-(
-* Parser to generate Desc from marked up input string
 * Core system to apply a collection of Matchers to an input string and pick the best
 * Tool to generate Descs etc from existing regression data
 * Rgeression suite
 -}
 
 main = runTestTT $ TestList
-        [ MatcherTests.tests
+        [ ParserTests.tests
+        , MatcherTests.tests
         ]
