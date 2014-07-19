c2e4b80e266a92e90a44f819676e450f1f964ee6
sdclibbery <sdclibbery@googlemail.com>
2014-07-19 22:38:55 +0100

    Update ToDo

--- RunTests.hs
+++ RunTests.hs
@@ -3,11 +3,10 @@ import qualified MatcherTests
 import qualified ParserTests
 
 {- TODO:
-x Need to support escaping of delimiters
-* File loader to load and parse a suite of matchers from a file
 * Core system to apply a collection of Matchers to an input string and pick the best
+* Main app to load and parse a suite of matchers from a file, and apply to an input string
 * Tool to generate Descs etc from existing regression data
-* Rgeression suite
+* Regression suite
 -}
 
 main = runTestTT $ TestList
