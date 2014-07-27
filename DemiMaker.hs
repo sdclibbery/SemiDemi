53cc7f4dde0ffff4c2d52b9cf1dc24774ca779e8
sdclibbery <sdclibbery@googlemail.com>
2014-07-27 20:05:16 +0100

    Minor refactor

--- DemiMaker.hs
+++ DemiMaker.hs
@@ -9,24 +9,24 @@ import Data.Aeson
 import Data.Maybe
 import Data.List
 import Data.Ord
+import Data.Function
 import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString.Lazy.Char8 as BS
 
 main = do
 	cases <- readFile "../SemiDemiData/testdata/testdata.json"
-	writeFile "../SemiDemiData/tvs.demi" $ process cases
+	writeFile "../SemiDemiData/tvs.demi" $ makeDemi cases
 
-process :: String -> String
-process = unlines . map output . sort . nubBy idsMatch . parse
+makeDemi :: String -> String
+makeDemi = unlines . map output . sort . nubBy ((==) `on` wurflId) . parseJson
 	where
-		idsMatch l r = wurflId l == wurflId r
 		output c = refUa c ++ "\t" ++ wurflId c
 
 data Case = Case { wurflId::String, refUa::String, testUa::String } deriving (Eq, Ord, Show)
 
-parse :: String -> [Case]
-parse = catMaybes . map (decode . BS.pack) . lines
+parseJson :: String -> [Case]
+parseJson = catMaybes . map (decode . BS.pack) . lines
 
 instance FromJSON Case where
     parseJSON (Object v) = Case <$> v .: "wurfl_id" <*> v .: "user_agent" <*> v .: "uagent"
--- RunTests.hs
+++ RunTests.hs
@@ -5,8 +5,8 @@ import qualified BestMatchTests
 import qualified DemiParserTests
 
 {- TODO:
-x Regression suite: automated tests
 * Refine Demi file generation to pass all regression tests...
+ * Try using Version for any numeric bits...
  * Finding Exact strings and then marking up version numbers should help...
 ? Do we really need better regression testing? EG auto generate version numbers QuickCheck style?
 -}
