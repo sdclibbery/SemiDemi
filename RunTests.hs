efa3e742a74bffc7cf07f0ba2a21fe138e1b9870
sdclibbery <sdclibbery@googlemail.com>
2014-08-03 23:55:48 +0100

    Planning

--- RunTests.hs
+++ RunTests.hs
@@ -5,8 +5,13 @@ import qualified BestMatchTests
 import qualified DemiParserTests
 
 {- TODO:
-x Remove 'empty' check in parser
+? Will we need to have version markup? How would this play with the single editDistance calculation?
+ ? Can we use the version markup in normalisation?
+* Matcher must keep a copy of the original string
 * In BestMatch, if multiple matches occur, do a single editDistance to find the best
+* Must have a test with two matchers differing by a single space, then match against a string differing by several version chars
+? Modify editDistance to score digit <-> digit differences less than other differences
+ ? Or 'normalise' by removing versions from both strings before editDistance matching...
 -}
 
 main = runTestTT $ TestList
