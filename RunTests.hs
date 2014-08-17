5a013412fbdafd44397871375a4e2dd16641b17a
sdclibbery <sdclibbery@googlemail.com>
2014-08-18 00:07:07 +0100

    Replanning after profiling

--- RunTests.hs
+++ RunTests.hs
@@ -6,6 +6,10 @@ import qualified DemiParserTests
 
 {- TODO:
 
+* profile/optimise; esp. scoring/normalisation
+ * Undo current changes and switch MatchString fully over to be ByteString instead.
+ * If still slow, run with -O2 (can setup sublime to do this?)
+
 * Try regression again, work through each failure in turn
  * Remove generic smarttv matcher from .demi file, but use it as the default result when nothing else matches
 
