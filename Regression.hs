36b70309a36d2539acc45cf87c17ef3e9e9989e4
sdclibbery <sdclibbery@googlemail.com>
2014-08-18 23:53:32 +0100

    Updated regression tests to use default matcher if there are no matches

--- Regression.hs
+++ Regression.hs
@@ -7,6 +7,7 @@ Description : Tool for running the Demi regression suite against the SemiDemi ma
 
 import DemiParser
 import BestMatch
+import qualified Matcher as M
 import Data.Either
 import Data.Aeson
 import Data.Maybe
@@ -17,27 +18,31 @@ import Control.Monad
 import qualified Data.ByteString.Lazy.Char8 as BS
 
 main = do
-	demi <- readFile "../SemiDemiData/tvs.demi"
-	tests <- readFile "../SemiDemiData/testdata/tvs.txt"
-	expected <- readFile "../SemiDemiData/testdata/testdata.json"
-	putStrLn $ regression demi expected tests
+    demi <- readFile "../SemiDemiData/tvs.demi"
+    tests <- readFile "../SemiDemiData/testdata/tvs.txt"
+    expected <- readFile "../SemiDemiData/testdata/testdata.json"
+    putStrLn $ regression demi expected tests
 
 
 regression :: String -> String -> String -> String
 regression d e t = case (parse d) of
-	(Right ms) -> (foldr (++) "" $ map (either (++ "\n\n") id) $ results ms) ++ "\nTotal: " ++ (show $ length ts) ++ "\nFailed: " ++ (show $ length $ lefts $ results ms)
-	(Left err) -> err
-	where
-		ts = lines t
-		es = map parseJson $ lines e
-		results ms = map (test ms es) $ zip ts [0..]
+    (Right ms) -> (foldr (++) "" $ map (either (++ "\n\n") id) $ results ms) ++ "\nTotal: " ++ (show $ length ts) ++ "\nFailed: " ++ (show $ length $ lefts $ results ms)
+    (Left err) -> err
+    where
+        ts = lines t
+        es = map parseJson $ lines e
+        results ms = map (test ms es) $ zip ts [0..]
 
 test :: [Matcher String] -> [String] -> (String, Int) -> Either String String
-test ms es (t, i) = do
-	m <- match t ms
-	if e == snd m then Right $ "" else Left $ "\nFAIL: " ++ t ++ "\n    EXPECTED: " ++ e ++ "\n         GOT: " ++ (snd m)
-	where
-		e = es !! i
+test ms es (t, i) = if e == snd m then Right $ "" else Left $ "\nFAIL: " ++ t ++ "\n    EXPECTED: " ++ e ++ "\n         GOT: " ++ (snd m)
+    where
+        m = matchWithDefault (M.Desc [] [], "generic_smarttv_browser") t ms
+        e = es !! i
+
+matchWithDefault :: Matcher String -> String -> [Matcher String] -> Matcher String
+matchWithDefault def t ms = case match t ms of
+    (Left err) -> def
+    (Right m) -> m
 
 data Case = Case { wurflId::String } deriving (Eq, Ord, Show)
 
