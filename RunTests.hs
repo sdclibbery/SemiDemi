b71e9893d6cc4f03b4298dd3ca3147a90456d684
sdclibbery <sdclibbery@googlemail.com>
2014-08-28 00:03:06 +0100

    Replan

--- RunTests.hs
+++ RunTests.hs
@@ -6,16 +6,23 @@ import qualified DemiParserTests
 
 {- TODO:
 
+* Lets get some stats on how many possible matchers we have to score for each UA?
+ * How can we reduce this number...
+
 * Optimise matching by building a tree of Exact/Disallowed
+ ! This a smallish optimisation now, but as the total number of matchers in the .demi file rises, the runtime will rise nonlinearly...
  * Split matching and scoring into separate files
  * Define and build a match tree
  * Then match by taking the UA string through the match tree to return a list of possible matches
 
+* ?How could we apply a similar global optimisation to scoring??
+ ? Could we build a 'prefix tree' for each leaf of the 'matcher tree'?
+  ? Could we then share some of the editDistance work between the possible matchers?
+  ? Alternatively, can we discard some of the possible matchers early this way??
+
 * Sort the 'head' in Regression.hs
  * Empty list means that there are NO matchers setup for this test yet (think TDD red)
  * More than one entry means multiple could have matched, and they should ALL be shown as possible expected matchers in the results...
-
-* ?How could we apply a similar global optimisation to scoring??
 
 * Work through and get all regression tests passing...           
 
